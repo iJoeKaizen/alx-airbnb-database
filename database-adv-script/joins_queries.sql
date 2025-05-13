5dba20bfc303e83be2a967ff5391c329878a50fa
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-05-13 00:09:25 +0000

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,37 @@
+--# INNER JOIN
+SELECT 
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date,
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    users.email
+FROM bookings
+INNER JOIN users ON bookings.users_id = users.id;
+
+
+--#LEFT JOIN
+SELECT 
+    properties.id AS property_id,
+    properties.name,
+    properties.location,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+--# FULL OUTER JOIN
+SELECT 
+    users.id AS user_id,
+    users.first_name,
+    users.email,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+
