a147122c25d50d04786b88c8c9a8f6e1fd65446f
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-05-13 01:09:15 +0000

    Create aggregations_and_window_functions.sql

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,18 @@
+-- BOOKINGS MADE BY EACH USER
+SELECT 
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    COUNT(bookings.id) AS total_bookings
+FROM users
+LEFT JOIN bookings ON users.id = bookings.user_id
+GROUP BY users.id, users.first_name, users.last_name;
+
+
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM bookings
+GROUP BY property_id;
+
