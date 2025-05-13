149459ae57b9f9339f20a7cfcd0d81995fd74bd1
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-05-13 00:55:10 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -37,3 +37,37 @@ SELECT
 FROM users
 FULL OUTER JOIN bookings ON users.id = bookings.user_id;
 
+ -- # NON-CORRELATED SUBQUERY
+SELECT 
+    id, 
+    name 
+FROM 
+    properties 
+WHERE 
+    id IN (
+        SELECT 
+            property_id 
+        FROM 
+            reviews 
+        GROUP BY 
+            property_id 
+        HAVING 
+            AVG(rating) > 4.0
+    );
+
+-- # CORRELATED SUBQUERY
+SELECT 
+    id, 
+    name 
+FROM 
+    users u
+WHERE 
+    (
+        SELECT 
+            COUNT(*) 
+        FROM 
+            bookings b 
+        WHERE 
+            b.user_id = u.id
+    ) > 3;
+
