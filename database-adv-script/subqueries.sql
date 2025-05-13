43b6549b10d7fc3548d3729676892ef480ce88dd
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-05-13 00:59:38 +0000

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,19 @@
+ -- # NON-CORRELATED SUBQUERY
+SELECT * 
+FROM properties
+WHERE id IN (
+    SELECT property_id 
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+
+-- # CORRELATED SUBQUERY
+SELECT * 
+FROM users u
+WHERE (
+    SELECT COUNT(*) 
+    FROM bookings b 
+    WHERE b.user_id = u.id
+) > 3;
